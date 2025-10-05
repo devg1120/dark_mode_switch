ff7e67d371bfe21f0da993d29904da7d55d729c0
devg1120 <devg1120@gmail.com>
2025-10-05 11:14:01 +0900

    up

--- src/App.vue
+++ src/App.vue
@@ -1,112 +1,123 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import DarkModeButton from './components/DarkModeButton.vue'
-import DarkModeSwitch from './components/DarkModeSwitch.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import DarkModeButton from "./components/DarkModeButton.vue";
+import DarkModeSwitch from "./components/DarkModeSwitch.vue";
 
-const isDarkModeEnabled = defineModel()
+const isDarkModeEnabled = defineModel();
 isDarkModeEnabled.value = false;
 
 function onSwitched(isSwitched) {
-  console.log('dark mode is enabled :', isSwitched);
+  console.log("dark mode is enabled :", isSwitched);
 }
 
 let css_dict_ = {
   darkmode: {
-     background : "#606060",
-     color: "#ffffff"
+    background: "#606060",
+    color: "#ffffff",
   },
   lightmode: {
-     background : "#ffffff",
-     color: "#000000"
-  }
-}
+    background: "#ffffff",
+    color: "#000000",
+  },
+};
 
 let css_dict = {
   darkmode: {
-     background : "red",
-     color: "#ffffff"
+    background: "red",
+    color: "#ffffff",
   },
   lightmode: {
-     background : "lightblue",
-     color: "#000000"
-  }
-}
+    background: "lightblue",
+    color: "#000000",
+  },
+};
 let css_dict2 = {
-  darkmode: {
-  },
-  lightmode: {
-  }
-}
+  darkmode: {},
+  lightmode: {},
+};
 let cssvar_dict = {
   darkmode: [
-    { name : "--main-background", value : "green" },
-    { name : "--main-color"     , value : "red" }
+    { name: "--main-background", value: "green" },
+    { name: "--main-color", value: "red" },
   ],
   lightmode: [
-    { name : "--main-background", value : "yellow" },
-    { name : "--main-color"     , value : "blue" }
-  ]
-}
+    { name: "--main-background", value: "yellow" },
+    { name: "--main-color", value: "blue" },
+  ],
+};
 </script>
 
 <template>
-<DarkModeButton  document_id="doc1"/>
+  <DarkModeButton document_id="doc1" />
   <div id="doc1">
-      <p>AAAAAA</p>
-      <p>AAAAAA</p>
-         <div>
-	   <h2>TEST TEST</h2>
-         </div>
+    <p>AAAAAA</p>
+    <p>AAAAAA</p>
+    <div>
+      <h2>TEST TEST</h2>
+    </div>
 
-         <table border="1" style="background-color:lightgray;">
-            <thead>
-              <tr><th></th><th>列-A</th><th>列-B</th></tr>
-            </thead>
-            <tbody>
-              <tr><th>行-1</th><td>A-1</td><td>B-1</td></tr>
-              <tr><th>行-2</th><td>A-2</td><td>B-2</td></tr>
-              <tr><th>行-3</th><td>A-3</td><td>B-3</td></tr>
-            </tbody>
-         </table>
-        
-      <p>AAAAAA</p>
+    <table border="1" style="background-color: lightgray">
+      <thead>
+        <tr>
+          <th></th>
+          <th>列-A</th>
+          <th>列-B</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>行-1</th>
+          <td>A-1</td>
+          <td>B-1</td>
+        </tr>
+        <tr>
+          <th>行-2</th>
+          <td>A-2</td>
+          <td>B-2</td>
+        </tr>
+        <tr>
+          <th>行-3</th>
+          <td>A-3</td>
+          <td>B-3</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <p>AAAAAA</p>
   </div>
-  
-<DarkModeButton  document_id="doc2" model="model1"/>
+
+  <DarkModeButton document_id="doc2" model="model1" />
   <div id="doc2">
-      <p>AAAAAA</p>
-      <p>AAAAAA</p>
-      <p>AAAAAA</p>
+    <p>AAAAAA</p>
+    <p>AAAAAA</p>
+    <p>AAAAAA</p>
   </div>
-  <DarkModeButton  document_id="doc3" :style_css="css_dict"/>
+  <DarkModeButton document_id="doc3" :style_css="css_dict" />
   <div id="doc3">
-      <p>AAAAAA</p>
-      <p>AAAAAA</p>
-      <p>AAAAAA</p>
+    <p>AAAAAA</p>
+    <p>AAAAAA</p>
+    <p>AAAAAA</p>
   </div>
 
-  <DarkModeButton  document_id="doc4"  :css_var="cssvar_dict"/>
+  <DarkModeButton document_id="doc4" :css_var="cssvar_dict" />
   <div id="doc4" class="target-element">
-      <p>AAAAAA</p>
-      <p>AAAAAA</p>
-      <p>AAAAAA</p>
+    <p>AAAAAA</p>
+    <p>AAAAAA</p>
+    <p>AAAAAA</p>
   </div>
-<!--
+  <!--
 <DarkModeSwitch @switched="onSwitched" v-model="isDarkModeEnabled"/>
--->
-</template>
+--></template>
 
-<style >
-
+<style>
 :root {
-    --main-background: yellow;
-    --main-color: blue;
-    --font-size: 16px;
+  --main-background: yellow;
+  --main-color: blue;
+  --font-size: 16px;
 }
 .target-element {
-    background: var(--main-background);
-    color: var(--main-color);
-    font-size: var(--font-size);
+  background: var(--main-background);
+  color: var(--main-color);
+  font-size: var(--font-size);
 }
-
 </style>
--- src/components/DarkModeButton.vue
+++ src/components/DarkModeButton.vue
@@ -1,11 +1,8 @@
 <script lang="ts">
 let cssvar_dict_default = {
-  darkmode: [
-  ],
-  lightmode: [
-  ] 
-} ;
-
+  darkmode: [],
+  lightmode: [],
+};
 </script>
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
@@ -29,10 +26,10 @@ const props = defineProps({
 */
 
 const props = defineProps({
-    model: { type: String, default: 'model2'},
-    document_id: String,
-    style_css:  { type: Object, default: cssElement},
-    css_var:  { type: Object }
+  model: { type: String, default: "model2" },
+  document_id: String,
+  style_css: { type: Object, default: cssElement },
+  css_var: { type: Object },
 });
 
 let doc = null;
@@ -50,62 +47,71 @@ if (toggle_color == "model1") {
   toggle_color_css = cssToggle2;
 }
 
-
 onMounted(() => {
   doc = document.getElementById(props.document_id);
   darkmode.value = false;
   disableDarkMode();
 });
 
-
 const darkmode = ref(false);
 
 function enableDarkMode() {
-    darkmode.value = true;
-    for (let [key, value] of Object.entries(toggle_color_css.darkmode)) {
-      toggle_ele.value.style.setProperty(`--${key}`, value);
-    }
-if (!props.css_var) {
+  darkmode.value = true;
+  for (let [key, value] of Object.entries(toggle_color_css.darkmode)) {
+    toggle_ele.value.style.setProperty(`--${key}`, value);
+  }
+  if (!props.css_var) {
     for (let [key, value] of Object.entries(props.style_css.darkmode)) {
       doc.style.setProperty(key, value);
     }
-} else {
-    for (let i = 0; i < props.css_var.darkmode.length; i++  ) {
-        let key = "";
-        let value = "";
-        for (let [k, v] of Object.entries(props.css_var.darkmode[i])) {
-	   if ( k == "name"  ) { key = v   ; continue }
-	   if ( k == "value" ) { value = v ; continue }
-	}
-	//console.log("setProperty", key, value);
-        doc.style.setProperty(key, value);
+  } else {
+    for (let i = 0; i < props.css_var.darkmode.length; i++) {
+      let key = "";
+      let value = "";
+      for (let [k, v] of Object.entries(props.css_var.darkmode[i])) {
+        if (k == "name") {
+          key = v;
+          continue;
+        }
+        if (k == "value") {
+          value = v;
+          continue;
+        }
+      }
+      //console.log("setProperty", key, value);
+      doc.style.setProperty(key, value);
     }
-}
   }
+}
 
 function disableDarkMode() {
-    darkmode.value = false;
-    for (let [key, value] of Object.entries(toggle_color_css.lightmode)) {
-      toggle_ele.value.style.setProperty(`--${key}`, value);
-    }
-if (!props.css_var) {
+  darkmode.value = false;
+  for (let [key, value] of Object.entries(toggle_color_css.lightmode)) {
+    toggle_ele.value.style.setProperty(`--${key}`, value);
+  }
+  if (!props.css_var) {
     for (let [key, value] of Object.entries(props.style_css.lightmode)) {
       doc.style.setProperty(key, value);
     }
-} else {
-    for (let i = 0; i < props.css_var.lightmode.length; i++  ) {
-        let key = "";
-        let value = "";
-        for (let [k, v] of Object.entries(props.css_var.lightmode[i])) {
-	   if ( k == "name"  ) { key = v   ; continue }
-	   if ( k == "value" ) { value = v ; continue }
-	}
-	//console.log("setProperty", key, value);
-        doc.style.setProperty(key, value);
+  } else {
+    for (let i = 0; i < props.css_var.lightmode.length; i++) {
+      let key = "";
+      let value = "";
+      for (let [k, v] of Object.entries(props.css_var.lightmode[i])) {
+        if (k == "name") {
+          key = v;
+          continue;
+        }
+        if (k == "value") {
+          value = v;
+          continue;
+        }
+      }
+      //console.log("setProperty", key, value);
+      doc.style.setProperty(key, value);
     }
-}
   }
-
+}
 </script>
 
 <template>
@@ -117,58 +123,64 @@ if (!props.css_var) {
       class="checkbox"
     />
     <label :for="checkbox_id" class="label transition">
-      <IconMoonFilled v-if="toggle_color == 'model1'" class="icon" style="color:#ffffff;"/>
-      <IconMoonFilled v-if="toggle_color == 'model2'" class="icon" style="color:#404040;"/>
-      <IconSunFilled  class="icon" style="color:#404040;"/>
+      <IconMoonFilled
+        v-if="toggle_color == 'model1'"
+        class="icon"
+        style="color: #ffffff"
+      />
+      <IconMoonFilled
+        v-if="toggle_color == 'model2'"
+        class="icon"
+        style="color: #404040"
+      />
+      <IconSunFilled class="icon" style="color: #404040" />
       <div :class="['toggle', 'transition', darkmode ? 'checked' : '']"></div>
     </label>
   </div>
 </template>
 
 <style scoped>
-  .container {
-    display: flex;
-    justify-content: start;
-    align-items: center;
-    z-index: 100;
-  }
-  .checkbox {
-    display: none;
-  }
-  .label {
-    width: 40px;
-    border-radius: 5px;
-    border: 1px solid var(--text-color);
-    padding: 3px 6px;
-    background: var(--bg-dark-mode-toggle);
-    cursor: pointer;
-    display: flex;
-    position: relative;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .icon {
-    font-size: 12px;
-  }
-  .toggle {
-    position: absolute;
-    background-color: var(--bg-secondary);
-    border-radius: 3px;
-    height: 20px;
-    width: 20px;
-    transform: translateX(0);
-  }
-  .toggle.checked {
-    transform: translateX(20px) !important;
-  }
-  
-  .transition {
-    transition: all 0.8s ease;
-    -webkit-transition: all 0.8s ease;
-    -moz-transition: all 0.8s ease;
-    -ms-transition: all 0.8s ease;
-    -o-transition: all 0.8s ease;
-  }
-  
-</style>
+.container {
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  z-index: 100;
+}
+.checkbox {
+  display: none;
+}
+.label {
+  width: 40px;
+  border-radius: 5px;
+  border: 1px solid var(--text-color);
+  padding: 3px 6px;
+  background: var(--bg-dark-mode-toggle);
+  cursor: pointer;
+  display: flex;
+  position: relative;
+  justify-content: space-between;
+  align-items: center;
+}
+.icon {
+  font-size: 12px;
+}
+.toggle {
+  position: absolute;
+  background-color: var(--bg-secondary);
+  border-radius: 3px;
+  height: 20px;
+  width: 20px;
+  transform: translateX(0);
+}
+.toggle.checked {
+  transform: translateX(20px) !important;
+}
 
+.transition {
+  transition: all 0.8s ease;
+  -webkit-transition: all 0.8s ease;
+  -moz-transition: all 0.8s ease;
+  -ms-transition: all 0.8s ease;
+  -o-transition: all 0.8s ease;
+}
+</style>
